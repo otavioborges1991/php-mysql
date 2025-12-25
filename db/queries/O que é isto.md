5b1599c6c734ea89c7db6450534573cf9401bbd4
Otávio Vínicios Borges <otavio.vinicios.borges@gmail.com>
2025-12-25 09:06:56 -0300

    projeto iniciado

---
+++ db/queries/O que é isto.md
@@ -0,0 +1,3 @@
+# O que é isto?
+
+Queries de MySQL usadas para criar o banco de dados deste projeto se encontram aqui.
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de jogos</title>
+    <link rel="stylesheet" href="/estilo/estilo.css">
+</head>
+<body>
+    <header>Header</header>
+    <main>
+        <h2>Escolha seu jogo</h2>
+    </main>
+    <footer>Footer</footer>
+</body>
+</html>
