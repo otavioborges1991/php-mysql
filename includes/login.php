a31a576e2272c562b91b915a5959fe7b6d822a98
Otávio Vínicios Borges <otavio.vinicios.borges@gmail.com>
2026-01-02 22:45:02 -0300

    Trabalhando no sistema de criptografia do banco de dados

--- includes/login.php
+++ includes/login.php
@@ -1,12 +1,23 @@
 <?php
 
+function cripto($senha):string{
+    // mais um nivel de encriptação para nosso sistema.
+    $cripto = '';
+    for ($posição=0; $posição<strlen($senha); $posição++) {
+        $letra = ord($senha[$posição]) + 1;
+        $cripto .= chr($letra);
+    }
+
+    return $cripto;
+}
+
 function gerar_hash($senha): string {
-    $hash = password_hash($senha, PASSWORD_DEFAULT);
+    $txt = cripto($senha);
+    $hash = password_hash($txt, PASSWORD_DEFAULT);
     return $hash;
 }
 
 function testar_hash($senha, $hash): bool {
-    $ok = password_verify($senha, $hash);
+    $ok = password_verify(cripto($senha), $hash);
     return $ok;
 }
-
